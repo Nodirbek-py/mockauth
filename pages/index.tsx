05f4c7c345b2d8eede6895eb0bef3411f2b1ea2f
Nodirbek Vositov <xesos2001@gmail.com>
2022-11-29 11:15:23 +0500

    [FEAT]: Homepage for the application is ready

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,6 @@
 import Head from 'next/head';
+import SyntaxHighlighter from 'react-syntax-highlighter';
+import { a11yDark } from 'react-syntax-highlighter/dist/esm/styles/hljs';
 
 export default function Home(): React.ReactNode {
     return (
@@ -10,7 +12,7 @@ export default function Home(): React.ReactNode {
             </Head>
             <header className='min-h-screen'>
                 <div className='h-12 shadow-md fixed w-screen backdrop-blur bg-white/0'>
-                    <nav className='max-w-7xl mx-auto h-full border-x flex items-center justify-between px-6'>
+                    <nav className='max-w-7xl mx-auto h-full flex items-center justify-between px-6'>
                         <div>
                             <h1 className='text-neutral-200 text-xl text-opacity-80 hover:text-opacity-100 cursor-pointer'>
                                 MockAuth
@@ -37,13 +39,23 @@ export default function Home(): React.ReactNode {
                         An Application for mocking authentication process. <br />
                         You can use this application as a backend for learning purposes.
                     </p>
+                    <p className='mb-4'>
+                        Powered by{' '}
+                        <a className='text-cyan-500' target='_blank' href='https://nextjs.org' rel='noreferrer'>
+                            Next.js
+                        </a>{' '}
+                        and{' '}
+                        <a href='https://www.mongodb.com' className='text-cyan-500' target='_blank' rel='noreferrer'>
+                            MongoDB
+                        </a>
+                    </p>
                     <button className='bg-cyan-500 px-4 py-2 rounded-md shadow-md shadow-cyan-500'>
                         <a href='#about'>Get Started</a>
                     </button>
                 </div>
             </header>
             <main>
-                <section className='max-w-7xl border min-h-screen mx-auto p-8' id='about'>
+                <section className='max-w-7xl min-h-screen mx-auto p-8' id='about'>
                     <h1 className='text-neutral-200 text-5xl mb-6 text-center'>About the project</h1>
                     <p className='text-neutral-200 font-thin text-xl text-center mb-10'>
                         I created this application in order to help guys who are learning development. When I was
@@ -75,12 +87,161 @@ export default function Home(): React.ReactNode {
                         </span>
                     </h1>
                 </section>
-                <section className='max-w-7xl border min-h-screen mx-auto p-8' id='docs'>
-                    
+                <section className='max-w-7xl min-h-screen mx-auto p-8' id='docs'>
+                    <h1 className='text-neutral-200 text-3xl mb-6'>Try it</h1>
+                    <p className='text-neutral-200 text-xl mb-6'>Run this code here, in a console or from any site:</p>
+                    <SyntaxHighlighter language='javascript' style={a11yDark}>
+                        {`
+fetch('https://mockauth.vercel.com/api/posts/', 
+    { method: 'GET', headers: { Authorization: 'Bearer <token>' }})
+        .then((res) => res.json())
+        .then((data) => console.log(data))
+                        `}
+                    </SyntaxHighlighter>
+                    <p className='text-neutral-200 text-xl my-4'>
+                        You can see that we have authorization here, we need token in order to send requests ang get
+                        desired response. To get this token you need to login to your account or register if you don
+                        {"'"}t have one. See the following snippet for registration
+                    </p>
+                    <SyntaxHighlighter language='javascript' style={a11yDark}>
+                        {`
+fetch('https://mockauth.vercel.com/api/auth/register', 
+    { 
+        method: 'POST',
+        body: JSON.stringify({username: 'john.doe', password: '*********', password2: '**********'})   
+    })
+        .then((res) => res.json())
+        .then((data) => console.log(data))
+                        `}
+                    </SyntaxHighlighter>
+                    <p className='text-neutral-200 text-xl my-4'>
+                        It has its own validation and error messages. Once you send correct request body you will get{' '}
+                        <code className='text-orange-300'>accessToken</code> and{' '}
+                        <code className='text-orange-300'>refreshToken</code>. So you can use these tokens in further
+                        steps.
+                    </p>
+                    <p className='text-neutral-200 text-xl my-4'>
+                        Here you can see how to use <code className='text-orange-300'>/login</code> endpoint
+                    </p>
+                    <SyntaxHighlighter language='javascript' style={a11yDark}>
+                        {`
+fetch('https://mockauth.vercel.com/api/auth/login', 
+    { 
+        method: 'POST',
+        body: JSON.stringify({username: 'john.doe', password: '*********'})   
+    })
+        .then((res) => res.json())
+        .then((data) => console.log(data))
+                        `}
+                    </SyntaxHighlighter>
+                    <p className='text-neutral-200 text-xl my-4'>
+                        One thing you have to keep in mind. There is one endpoint{' '}
+                        <code className='text-orange-300'>/private</code>. Which requires one thing additional to{' '}
+                        <code className='text-orange-300'>accesToken</code>. It is{' '}
+                        <code className='text-orange-300'>isAdmin</code> property which is after decoding token. It will
+                        be <code className='text-orange-300'>true</code> or{' '}
+                        <code className='text-orange-300'>false</code>. You can get those datas from private endpoint
+                        only if you are logged in as admin (that property will be true). And there is only one admin
+                        user, here are the credentials <br />
+                        <code className='text-orange-300'>
+                            username: <b>adminuser</b>
+                        </code>
+                        <br />
+                        <code className='text-orange-300'>
+                            password: <b>adminpassword</b>
+                        </code>
+                    </p>
+                    <p className='text-neutral-200 text-xl my-4'>
+                        Here are the sample responses from various endpoints
+                        <br />
+                        <code className='text-orange-300'>/private</code>:
+                        <SyntaxHighlighter language='json' style={a11yDark}>
+                            {`
+{
+    "posts": [
+        {
+            "_id": "6381050c3255440503fa839d",
+            "firstName": "Tabb",
+            "lastName": "Swarbrigg",
+            "dob": "31/01/1988",
+            "address": "8 Knutson Lane",
+            "phoneNumber": "379-558-9861",
+            "bankAccount": {
+                "bankName": "Greenholt-Watsica",
+                "iban": "AZ60 QOOZ SBWZ D3TY SMSD HFXC 9GQW",
+                "country": "Brazil",
+                "_id": "6381050c3255440503fa839e"
+            },
+            "__v": 0
+        },
+        ...
+    ]
+}
+                        `}
+                        </SyntaxHighlighter>
+                        <code className='text-orange-300'>/posts</code>:
+                        <SyntaxHighlighter language='json' style={a11yDark}>
+                            {`
+{
+    "posts": [
+        {
+            "_id": "6380ea7639cd79f3d505ad2b",
+            "title": "Post title 1",
+            "body": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+            "__v": 0
+        },
+        ...
+    ]
+}
+`}
+                        </SyntaxHighlighter>
+                        <code className='text-orange-300'>/posts/:_id</code>:
+                        <p>
+                            You need to send <code className='text-orange-300'>_id</code> property of the post in order
+                            to get single post as a response
+                        </p>
+                        <SyntaxHighlighter language='json' style={a11yDark}>
+                            {`
+{
+    "posts": [
+        {
+            "_id": _id,
+            "title": "Post title 1",
+            "body": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+            "__v": 0
+        },
+        ...
+    ]
+}
+`}
+                        </SyntaxHighlighter>
+                        <code className='text-orange-300'>/auth/login</code> or{' '}
+                        <code className='text-orange-300'>/auth/register</code>:
+                        <SyntaxHighlighter language='json' style={a11yDark}>
+                            {`
+{
+    "message": "Welcome",
+    "accessToken": "<accessToken>",
+    "refreshToken": "<refreshToken>"
+}
+`}
+                        </SyntaxHighlighter>
+                    </p>
                 </section>
             </main>
 
-            <footer></footer>
+            <footer className='border-t border-neutral-600 py-6 px-4'>
+                <p className='text-center text-neutral-200'>
+                    Made with ❤️ and{' '}
+                    <a className='text-cyan-400' href='https://nextjs.org' target='_blank' rel='noreferrer'>
+                        Next.js
+                    </a>{' '}
+                    by{' '}
+                    <a className='text-cyan-400' href='https://github.com/nodirbek-py' target='blank' rel='noreferrer'>
+                        Nodirbek Vositov
+                    </a>
+                </p>
+            </footer>
         </div>
     );
 }
